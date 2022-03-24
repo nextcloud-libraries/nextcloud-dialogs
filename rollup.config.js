a4439d8efdf975f93311c9ccce8d0564b677cfeb
Tobias Speicher <rootcommander@gmail.com>
2022-03-24 16:26:50 +0100

    Replace deprecated String.prototype.substr()
    .substr() is deprecated so we replace it with .slice() which works similarily but isn't deprecated
    
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -16,17 +16,17 @@ const packageJson = require("./package.json");
 const translations = fs
 	.readdirSync('./l10n')	
 	.filter(name => name !== 'messages.pot' && name.endsWith('.pot'))
-	.map(file => {	
-		const path = './l10n/' + file	
-		const locale = file.substr(0, file.length - '.pot'.length)	
+	.map(file => {
+		const path = './l10n/' + file
+		const locale = file.slice(0, -'.pot'.length)
 
 
 		const po = fs.readFileSync(path)	
 		const json = gettextParser.po.parse(po)	
-		return {	
-			locale,	
-			json,	
-		}	
+		return {
+			locale,
+			json,
+		}
 	})
 
 export default {
