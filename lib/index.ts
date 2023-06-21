1dbf555edf3f40306272f8f8027cf0f4859912d5
Ferdinand Thiessen <opensource@fthiessen.de>
2023-06-21 03:44:40 +0200

    fix: Setup Typescript for node16 resolution of `@nextcloud/l10n`
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export { FilePicker, FilePickerType, FilePickerBuilder, getFilePickerBuilder } from './filepicker'
-export { TOAST_UNDO_TIMEOUT, TOAST_DEFAULT_TIMEOUT, TOAST_PERMANENT_TIMEOUT } from './toast'
-export { TOAST_ARIA_LIVE_OFF, TOAST_ARIA_LIVE_POLITE, TOAST_ARIA_LIVE_ASSERTIVE } from './toast'
-export { showMessage, showSuccess, showWarning, showInfo, showError, showUndo } from './toast'
+export { FilePicker, FilePickerType, FilePickerBuilder, getFilePickerBuilder } from './filepicker.js'
+export { TOAST_UNDO_TIMEOUT, TOAST_DEFAULT_TIMEOUT, TOAST_PERMANENT_TIMEOUT } from './toast.js'
+export { TOAST_ARIA_LIVE_OFF, TOAST_ARIA_LIVE_POLITE, TOAST_ARIA_LIVE_ASSERTIVE } from './toast.js'
+export { showMessage, showSuccess, showWarning, showInfo, showError, showUndo } from './toast.js'
