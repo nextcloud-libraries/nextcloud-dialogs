5956b15aaf79df6fac10f09795354946210a25fb
Ferdinand Thiessen <opensource@fthiessen.de>
2025-02-28 11:01:19 +0100

    chore: simplify vitest config and fix some build issues
    Fixed issues:
    - When building with `dev` we were not enabling vue-devtools.
      This is fixed by setting `NODE_ENV=development`.
    - The tsconfig rootdir was not aligned with the `includes` section.
      All included files need to be within the root dir.
      So we now have one config for production code and one for
      setup and tests.
    - The config option "rollup types" will create independent type,
      this causes the filepicker module to be incompatible with the index
       module we exported. Fixed by not rolling up types (note: roll up
       types only works when there is only a single entry point)
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@
 import { createLibConfig } from '@nextcloud/vite-config'
 import { readdirSync, readFileSync } from 'fs'
 import { po as poParser } from 'gettext-parser'
-import { defineConfig, type UserConfigFn } from 'vite'
 
 const translations = readdirSync('./l10n')
 	.filter(name => name !== 'messages.pot' && name.endsWith('.pot'))
@@ -21,40 +20,34 @@ const translations = readdirSync('./l10n')
 		}
 	})
 
-export default defineConfig((env) => {
-	return createLibConfig({
-		index: 'lib/index.ts',
-		filepicker: 'lib/filepicker.ts',
-	}, {
-		config: {
-			build: {
-				// Fix for vite config, TODO: remove with next release
-				cssCodeSplit: false,
-			},
+export default createLibConfig({
+	index: 'lib/index.ts',
+	filepicker: 'lib/filepicker.ts',
+}, {
+	config: {
+		build: {
+			// Fix for vite config, TODO: remove with next release
+			cssCodeSplit: false,
 		},
-		// We build for ESM and legacy common js
-		libraryFormats: ['es', 'cjs'],
-		// We want one single output CSS file
-		inlineCSS: false,
-		// Packages that should be externalized or bundled
-		nodeExternalsOptions: {
-			// for subpath imports like '@nextcloud/l10n/gettext'
-			include: [/^@nextcloud\//],
-			exclude: [
-				// we should not rely on external vue SFC dependencies thus bundle all .vue files
-				/^vue-material-design-icons\//,
-				/\.vue(\?|$)/,
-				// and bundle raw data, e.g., raw SVGs
-				/\?raw$/,
-			],
-		},
-		// Inject our translations
-		replace: {
-			__TRANSLATIONS__: JSON.stringify(translations),
-		},
-		// Rollup our type definitions to only publish what it needed
-		DTSPluginOptions: {
-			rollupTypes: env.mode === 'production',
-		},
-	})(env)
-}) as UserConfigFn
+	},
+	// We build for ESM and legacy common js
+	libraryFormats: ['es', 'cjs'],
+	// We want one single output CSS file
+	inlineCSS: false,
+	// Packages that should be externalized or bundled
+	nodeExternalsOptions: {
+		// for subpath imports like '@nextcloud/l10n/gettext'
+		include: [/^@nextcloud\//],
+		exclude: [
+			// we should not rely on external vue SFC dependencies thus bundle all .vue files
+			/^vue-material-design-icons\//,
+			/\.vue(\?|$)/,
+			// and bundle raw data, e.g., raw SVGs
+			/\?raw$/,
+		],
+	},
+	// Inject our translations
+	replace: {
+		__TRANSLATIONS__: JSON.stringify(translations),
+	},
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,42 +2,33 @@
  * SPDX-FileCopyrightText: 2023-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: CC0-1.0
  */
-import type { ConfigEnv } from 'vite'
-import { defineConfig, type ViteUserConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
 import config from './vite.config'
 
-export default defineConfig(async (env: ConfigEnv): Promise<ViteUserConfig> => {
-	const cfg = await config(env)
-
-	return {
-		...cfg,
-
-		// filter node-externals which will interfere with vitest
-		plugins: cfg.plugins!.filter((plugin) => plugin && (!('name' in plugin) || plugin.name !== 'node-externals')),
-
-		// vitest configuration
-		test: {
-			environment: 'happy-dom',
-			coverage: {
-				all: true,
-				provider: 'v8',
-				include: ['lib/**/*.ts', 'lib/*.ts'],
-				exclude: ['lib/**/*.spec.ts'],
-			},
-			css: {
-				modules: {
-					classNameStrategy: 'non-scoped',
-				},
-			},
-			setupFiles: 'test/setup.ts',
-			server: {
-				deps: {
-					inline: [
-						/@nextcloud\/vue/, // Fix unresolvable .css extension for ssr
-						/@nextcloud\/files/, // Fix CommonJS cancelable-promise not supporting named exports
-					],
-				},
+const testConfig = defineConfig({
+	test: {
+		environment: 'happy-dom',
+		coverage: {
+			all: true,
+			provider: 'v8',
+			include: ['lib/**/*.ts', 'lib/*.ts'],
+			exclude: ['lib/**/*.spec.ts'],
+		},
+		css: {
+			modules: {
+				classNameStrategy: 'non-scoped',
 			},
 		},
-	}
+		setupFiles: 'test/setup.ts',
+		server: {
+			deps: {
+				inline: [
+					/@nextcloud\/vue/, // Fix unresolvable .css extension for ssr
+					/@nextcloud\/files/, // Fix CommonJS cancelable-promise not supporting named exports
+				],
+			},
+		},
+	},
 })
+
+export default defineConfig(async (...args) => mergeConfig(await config(...args), testConfig))
